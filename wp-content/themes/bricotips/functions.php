cea8b6c278d3fe76116da5e297255fe7ef0770ef
Jérôme OLLIVIER <54033192+Spirecool@users.noreply.github.com>
2023-03-25 15:45:58 +0100

    Adding hooks

--- wp-content/themes/bricotips/functions.php
+++ wp-content/themes/bricotips/functions.php
@@ -2,7 +2,8 @@
 
 // Action qui permet de charger des scripts dans notre thème
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
-function theme_enqueue_styles(){
+function theme_enqueue_styles()
+{
     // Chargement du style.css du thème parent Twenty Twenty
     wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
     // Chargement du css/theme.css pour nos personnalisations
@@ -14,7 +15,6 @@ function theme_enqueue_styles(){
 
     // Chargement du /css/shortcodes/banniere-titre.css pour notre shortcode banniere titre
     wp_enqueue_style('banniere-titre-shortcode', get_stylesheet_directory_uri() . '/css/shortcodes/banniere-titre.css', array(), filemtime(get_stylesheet_directory() . '/css/shortcodes/banniere-titre.css'));
-
 }
 
 /* CHARGEMENT DES WIDGETS */
@@ -57,13 +57,13 @@ function banniere_titre_func($atts)
     ob_start();
 
     if ($atts['src'] != "") {
-        ?>
+?>
 
         <div class="banniere-titre" style="background-image: url(<?= $atts['src'] ?>)">
             <h2 class="titre"><?= $atts['titre'] ?></h2>
         </div>
 
-        <?php
+    <?php
     }
 
     //J'arrête de récupérer le flux d'information et le stock dans la fonction $output
@@ -72,3 +72,102 @@ function banniere_titre_func($atts)
 
     return $output;
 }
+
+/* HOOK FILTERS */
+
+
+function the_title_filter($title)
+{
+    if (is_single() && in_category('outils') && in_the_loop()) {
+        return 'Outil: ' . $title;
+    }
+    return $title;
+}
+
+add_filter('the_title', 'the_title_filter');
+
+
+add_filter('get_the_archive_title', function ($title) {
+    if (is_category()) {
+        $title = "Liste des " . strtolower(single_cat_title('', false));
+    }
+    return $title;
+});
+
+
+function the_category_filter($categories)
+{
+    return str_replace("Outils", "Tous les outils", $categories);
+}
+
+add_filter('the_category', 'the_category_filter');
+
+
+function the_content_filter($content)
+{
+    if (is_single() && in_category('outils')) {
+        return '<hr><h2>Description</h2>' . $content;
+    }
+    return $content;
+}
+
+add_filter('the_content', 'the_content_filter');
+
+
+
+function the_excerpt_filter($content)
+{
+    if (is_archive()) {
+        return $content . "<div class='more-excerpt'><a href='" . get_the_permalink() . "'>En savoir plus sur l'outil</a></div>";
+    }
+    return $content;
+}
+
+add_filter('the_excerpt', 'the_excerpt_filter');
+
+
+// Modification de la pagination
+function paginate_links_filter($r)
+{
+    return "Pages : " . $r;
+}
+
+add_filter('paginate_links_output', 'paginate_links_filter');
+
+
+
+
+/* HOOKS ACTIONS */
+
+
+
+function loop_end_action()
+{
+    if (is_archive()) :
+    ?>
+        <p class="after-loop">
+            <?php
+            echo do_shortcode('[banniere-titre src="http://localhost/bricotips-oc/wp-content/uploads/2023/03/banniere-image.webp" titre="BricoTips"]');
+            ?>
+        </p>
+    <?php
+    endif;
+}
+
+add_action('loop_end', 'loop_end_action');
+
+// on affiche ce texte uniquement sur le premier article
+$shown = false;
+function bricotips_intro_section_action()
+{
+    global $shown;
+    if (is_archive() && !$shown) :
+    ?>
+        <p class="intro">Vous trouverez dans cette page la liste de tous les outils que nous avons référencée pour le
+            moment. La liste n'est pas exhaustive, mais s'enrichira au fur et à mesure.</p>
+<?php
+        $shown = true;
+    endif;
+}
+
+add_action('bricotips_intro_section', 'bricotips_intro_section_action');
